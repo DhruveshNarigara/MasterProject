cbb26c8336eba39e98fa6c8611adc9fdd08176e9
HinalVaghasiya <hinalvaghasiya589@gmail.com>
2024-02-26 15:18:42 +0530

    dfghj

--- MasterProject/Repositories/DepartmentRepositories.cs
+++ MasterProject/Repositories/DepartmentRepositories.cs
@@ -14,5 +14,39 @@ namespace MasterProject.Repositories
         {
             
         }
+
+        public void UpdateEmployees(DepartmentModel department)
+        {
+            conn.Open();
+            using (var cmd = new NpgsqlCommand("UPDATE t_deptmaster SET c_deptname = @deptname WHERE c_deptid = @deptId", conn))
+            {
+                cmd.Parameters.AddWithValue("@deptId", department.c_deptid);
+                cmd.Parameters.AddWithValue("@deptname", department.c_deptname);
+
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
+        }
+
+        public void DeleteEmployees(int deptId)
+        {
+            conn.Open();
+            using (var cmd = new NpgsqlCommand("DELETE FROM t_city WHERE c_cityid = @cityId", conn))
+            {
+                cmd.Parameters.AddWithValue("@cityId", deptId);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine($"{rowsAffected} row(s) deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("No rows deleted.");
+                }
+            }
+            conn.Close();
+        }
+
+
     }
 }
