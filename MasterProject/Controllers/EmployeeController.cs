9d8e771f4c76200c96e37bdb2543f07807c61312
MehtaP1489 <mehtapreya25@gmail.com>
2024-02-26 15:29:26 +0530

    fetch emp repo

--- MasterProject/Controllers/EmployeeController.cs
+++ MasterProject/Controllers/EmployeeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MasterProject.Models;
+
+
 
 namespace MasterProject.Controllers
 {
