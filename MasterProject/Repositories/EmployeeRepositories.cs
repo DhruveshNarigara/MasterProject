b9304e8a828fcb07d44a8e08ad0e23b249993b07
devarsh7929 <apitestmail20@gmail.com>
2024-02-27 12:35:05 +0530

    abc

--- MasterProject/Repositories/EmployeeRepositories.cs
+++ MasterProject/Repositories/EmployeeRepositories.cs
@@ -10,7 +10,7 @@ namespace MasterProject.Repositories
         public IEnumerable<EmployeeModel> GetAllEmployees()
         {
             var employees = new List<EmployeeModel>();
-            string sql = "SELECT c_empid, c_uid, c_empname, c_empgender, c_empdob, c_empshift, c_empdept, c_empimage FROM t_employeemaster";
+            string sql = "SELECT c_empid, c_empname, c_empgender, c_empdob, c_empshift, c_empdept, c_empimage FROM t_employeemaster";
 
             using (var command = new NpgsqlCommand(sql, conn))
             {
@@ -21,7 +21,7 @@ namespace MasterProject.Repositories
                     var employee = new EmployeeModel
                     {
                         c_empid = reader.GetInt32(reader.GetOrdinal("c_empid")),
-                        c_uid = reader.GetInt32(reader.GetOrdinal("c_uid")),
+                       // c_uid = reader.GetInt32(reader.GetOrdinal("c_uid")),
                         c_empname = reader.GetString(reader.GetOrdinal("c_empname")),
                         c_empgender = reader.GetString(reader.GetOrdinal("c_empgender")),
                         c_empdob = reader.GetDateTime(reader.GetOrdinal("c_empdob")),
@@ -41,7 +41,7 @@ namespace MasterProject.Repositories
         public EmployeeModel GetEmployeeById(int id)
         {
             EmployeeModel employee = null;
-            string sql = "SELECT c_empid, c_uid, c_empname, c_empgender, c_empdob, c_empshift, c_empdept, c_empimage FROM t_employeemaster WHERE c_empid = @id";
+            string sql = "SELECT c_empid, c_empname, c_empgender, c_empdob, c_empshift, c_empdept, c_empimage FROM t_employeemaster WHERE c_empid = @id";
 
             using (var command = new NpgsqlCommand(sql, conn))
             {
@@ -54,7 +54,7 @@ namespace MasterProject.Repositories
                     employee = new EmployeeModel
                     {
                         c_empid = reader.GetInt32(reader.GetOrdinal("c_empid")),
-                        c_uid = reader.GetInt32(reader.GetOrdinal("c_uid")),
+                       // c_uid = reader.GetInt32(reader.GetOrdinal("c_uid")),
                         c_empname = reader.GetString(reader.GetOrdinal("c_empname")),
                         c_empgender = reader.GetString(reader.GetOrdinal("c_empgender")),
                         c_empdob = reader.GetDateTime(reader.GetOrdinal("c_empdob")),
@@ -72,11 +72,11 @@ namespace MasterProject.Repositories
         public void AddEmployee(EmployeeModel employee)
         {
             conn.Open();
-            string sql = "INSERT INTO t_employeemaster (c_uid, c_empname, c_empgender, c_empdob, c_empshift, c_empdept, c_empimage) VALUES (@uid, @name, @gender, @dob, @shift, @dept, @image)";
+            string sql = "INSERT INTO t_employeemaster ( c_empname, c_empgender, c_empdob, c_empshift, c_empdept, c_empimage) VALUES ( @name, @gender, @dob, @shift, @dept, @image)";
                 
             using (var command = new NpgsqlCommand(sql, conn))
             {
-                command.Parameters.AddWithValue("@uid", employee.c_uid);
+                //command.Parameters.AddWithValue("@uid", employee.c_uid);
                 command.Parameters.AddWithValue("@name", employee.c_empname);
                 command.Parameters.AddWithValue("@gender", employee.c_empgender);
                 command.Parameters.AddWithValue("@dob", employee.c_empdob);
@@ -91,11 +91,11 @@ namespace MasterProject.Repositories
 
         public void UpdateEmployee(EmployeeModel employee)
         {
-            string sql = "UPDATE t_employeemaster SET c_uid = @uid, c_empname = @name, c_empgender = @gender, c_empdob = @dob, c_empshift = @shift, c_empdept = @dept, c_empimage = @image WHERE c_empid = @id";
+            string sql = "UPDATE t_employeemaster SET  c_empname = @name, c_empgender = @gender, c_empdob = @dob, c_empshift = @shift, c_empdept = @dept, c_empimage = @image WHERE c_empid = @id";
  conn.Open();
             using (var command = new NpgsqlCommand(sql, conn))
             {
-                command.Parameters.AddWithValue("@uid", employee.c_uid);
+                //command.Parameters.AddWithValue("@uid", employee.c_uid);
                 command.Parameters.AddWithValue("@name", employee.c_empname);
                 command.Parameters.AddWithValue("@gender", employee.c_empgender);
                 command.Parameters.AddWithValue("@dob", employee.c_empdob);
